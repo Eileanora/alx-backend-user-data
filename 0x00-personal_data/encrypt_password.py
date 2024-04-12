9fa9f772b83604f0a08aabb0f8412503c4c75448
yasmeen <yasmeenhany14@gmail.com>
2024-04-12 03:26:15 +0200

    task 6" " "

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -6,3 +6,8 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     '''Returns a salted, hashed password.'''
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    '''Checks if a password matches a given hash.'''
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -50,16 +50,17 @@ def get_logger() -> logging.Logger:
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
     '''Returns a connector to a MySQL database.'''
-    username = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
-    password = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
+    username = os.getenv('PERSONAL_DATA_DB_USERNAME', "root")
+    password = os.getenv('PERSONAL_DATA_DB_PASSWORD', "")
     host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
-    db_name = os.getenv('PERSONAL_DATA_DB_NAME')
+    db_name = os.getenv('PERSONAL_DATA_DB_NAME', "")
 
-    return mysql.connector.connection.MySQLConnection(
+    return mysql.connector.connect(
         user=username,
         password=password,
         host=host,
-        database=db_name)
+        database=db_name
+    )
 
 
 if __name__ == '__main__':
