1fe4084095d4dc3347b9c2d235f391569a9ede1e
yasmeen <yasmeenhany14@gmail.com>
2024-04-12 03:22:30 +0200

    task 5

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''Module for storing the encrypt_password function.'''
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    '''Returns a salted, hashed password.'''
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
